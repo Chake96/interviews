ed23e8a4805b4a898adbc1bec8865596281c0d98
Chake96 <carsonhake@gmail.com>
2022-07-28 14:50:01 -0400

    updated

--- math/numbers.h
+++ math/numbers.h
@@ -6,7 +6,12 @@ const std::vector<std::vector<Integer>>& pascals(size_t n){
     static std::vector<std::vector<Integer>> triangle(n, std::vector<Integer>(n, {}));
     static bool init{false};
     static size_t max_n = 3;
-    if(n > triangle.capacity()){
+    //TODO: fix this by preallocating twice, allocation of rows should be previous row_size + 1,
+    // then fill in first and last with 1
+
+
+
+    if(n > triangle.capacity() || !init){
         auto triangle_cap = triangle.capacity();
         triangle.resize(n*2, std::vector<int>(n));
         for(; triangle_cap < triangle.capacity(); triangle_cap++){
@@ -31,14 +36,9 @@ const std::vector<std::vector<Integer>>& pascals(size_t n){
             }
         }
     }else if(n > max_n){
-        //TODO: fix segfault
-        for(size_t row = max_n; max_n < n; row++){
+        for(size_t row = max_n-1; max_n < n; row++){
             for(auto i = 0; i <= row; i++){
-                if(row == i || i == 0) {
-                    triangle[row][i] = 1;
-                }else{
-                    triangle[row][i] = triangle[row-1][i-1] + triangle[row-1][i];
-                }
+                triangle[row][i] = triangle[row-1][i-1] + triangle[row-1][i];
             }
         }
         max_n = n;
